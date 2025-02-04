0105b853f4ee188e7f1c2ab56c8793e4eded72d3
Xue Ning <s10267391@connect.np.edu.sg>
2025-02-04 14:48:08 +0800

    fixed image links

--- js/product-details.js
+++ js/product-details.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const targetProduct = document.querySelector(".pro img[src='images/HSR Products/Small Plush.webp']");
+    const targetProduct = document.querySelector(".pro img[src='images/HSR Products/Small-Plush.webp']");
 
     if (targetProduct) {
         targetProduct.closest(".pro").addEventListener("click", function (event) {
